9c0ae1ae88b521783657b4a640da1842ec5aa7e7
Blessen 25 <blessenvinoymathew25@gmail.com>
2025-04-01 22:58:29 +0530

    Commited Changes

--- frontend/geelong_church_project/src/components/header/header.tsx
+++ frontend/geelong_church_project/src/components/header/header.tsx
@@ -6,13 +6,22 @@ const Header = () => {
     return(
         
         <>
-            <div id="header">
+            <nav id="header">
                 <div className="headerimg">
                     <a href="#">
-                        <img src="../assets/images/Logo/Logoimg-close.jpeg" alt="" />
+                        <img src="../assets/images/Logo/Logoimg-close.jpg" alt="" />
                     </a>
                 </div>
-            </div>
+                <div className="headercontents">
+                    <a href='#' className='headercontentspara'>Home</a>
+                    <a href='#' className='headercontentspara'>About</a>
+                    <a href='#' className='headercontentspara'>Ministries</a>
+                    <a href='#' className='headercontentspara'>Newsletters</a>
+                    <a href='#' className='headercontentspara'>Church Online</a>
+                    <a href='#' className='headercontentspara'>Contact</a>
+                    
+                </div>
+            </nav>
         </>
     )
 }
---
+++ frontend/geelong_church_project/src/constants.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ImportantConstants = {
+    
+    'background-color' : '#f2f0f1',
+    'primary-color' : '#3c79b0',
+};
