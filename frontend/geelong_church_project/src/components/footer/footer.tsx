b16ac69442671b88a99d9174c0f61ba5b4433873
Blessen 25 <blessenvinoymathew25@gmail.com>
2025-04-12 08:14:57 +0530

    Commited Changes

--- frontend/geelong_church_project/src/components/footer/footer.tsx
+++ frontend/geelong_church_project/src/components/footer/footer.tsx
@@ -7,7 +7,15 @@ export const Footer = () => {
         <>  
             <div id='footer'>
                <div className="footerdiv1">
-                    <img src="./assets/images/Logo/Logo_new.png" alt="" />
+                    <div className="footerdiv1img">
+                        <a href="#">
+                          <img src="./assets/images/Logo/Logo_new.png" alt="" />
+                        </a>
+                    </div>
+                    
+                    <div className="footerdiv1text">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta voluptate a placeat debitis. Voluptatum maiores incidunt esse quod, molestiae asperiores eaque numquam nostrum. Deserunt, itaque. Ipsum ad officiis quaerat rerum.</p>
+                    </div>
                </div>
                <div className="footerdiv2">
 
