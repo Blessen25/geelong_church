2ca042523512efe9c6b37f6d9649844e56aed9e7
Blessen Vinoy Mathew <blessenvinoymathew25@gmail.com>
2025-05-20 13:58:59 +0530

    Commited Changes

--- frontend/geelong_church_project/src/array.tsx
+++ frontend/geelong_church_project/src/array.tsx
@@ -3,7 +3,7 @@ import { text } from "stream/consumers"
 
 export const HomeFourDivChildArray = [
 
-    {route:"#",text:"Verse of the Week",image:"../assets/images/main/church.jpg"},
+    {route:"#",text:"Bible Verses",image:"../assets/images/main/church.jpg"},
     {route:"/newsletters",text:"Upcoming Events",image:"../assets/images/main/cross_two.jpg"},
     {route:"/contact",text:"Need Prayer?",image:"../assets/images/main/cross.jpg"},
     {route:"/about",text:"Our Mission",image:"../assets/images/main/jesus-neon.jpg"},
