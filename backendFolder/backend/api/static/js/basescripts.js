84c01db557320b456c59a28bfd3ec537faa2e024
Blessen Vinoy Mathew <blessenvinoymathew25@gmail.com>
2025-06-06 14:36:45 +0530

    Commited Changes

--- backendFolder/backend/api/static/js/basescripts.js
+++ backendFolder/backend/api/static/js/basescripts.js
@@ -15,6 +15,17 @@ function closeDeleteModal() {
     document.getElementById('deleteModalOverlay').style.display = 'none';
 }
 
+/* Conversion of date */
+
+function converttoDDMMYYYY(dateString){
+    
+    const date = new Date(dateString);
+    const day = String(date.getDate()).padStart(2,'0');
+    const month = String(date.getMonth() + 1).padStart(2,'0');
+    const year = date.getFullYear();
+    return `${year}-${month}-${day}`
+}
+
 
 /* EDIT JS */
 
@@ -22,7 +33,13 @@ function showEditModal(Id,name, date){
 
     document.getElementById('editEventId').value=Id;
     document.getElementById('editEventName').value=name;
-    document.getElementById('editEventDate').value=date;
+    document.getElementById('editEventDate').value=converttoDDMMYYYY(date);
     document.getElementById('EditModel').style.display = 'block'; 
     document.getElementById('EditModalOverlay').style.display = 'block';
+}
+
+function closeEditModal(){
+
+    document.getElementById('EditModel').style.display='none';
+    document.getElementById('EditModalOverlay').style.display='none';
 }
