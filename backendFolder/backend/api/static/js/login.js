fe5d6a9f3b441d914f68e9e2d10dc2473d1e40cd
Blessen Vinoy Mathew <blessenvinoymathew25@gmail.com>
2025-06-11 21:09:14 +0530

    Commited Changes

--- backendFolder/backend/api/static/js/login.js
+++ backendFolder/backend/api/static/js/login.js
@@ -1,1 +1,11 @@
-console.log("show")
+const togglePassword = document.getElementById('typePassswordid');
+const passwordInput = document.getElementById('password');
+
+togglePassword.addEventListener('click',function(){
+
+    const isPassword = passwordInput.type === 'password';
+    passwordInput.type = isPassword ? 'text' : 'password';
+
+    this.classList.toggle('fa-eye');
+    this.classList.toggle('fa-eye-slash');
+})
