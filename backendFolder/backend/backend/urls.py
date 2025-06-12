79b02fdf70c9771d9ecc221036654dd51520953c
Blessen Vinoy Mathew <blessenvinoymathew25@gmail.com>
2025-06-12 09:59:43 +0530

    Commited Changes

--- backendFolder/backend/backend/urls.py
+++ backendFolder/backend/backend/urls.py
@@ -30,9 +30,9 @@ urlpatterns = [
     path('login/',User_login, name='login'),
     path('logout/',User_logout, name='logout'),
     
-    path('password-reset/',auth_views.PasswordResetView.as_view(template_name='registration/password_reset.html'),name='password_reset'),
-    path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='registation/password_reset_done.html'),name='password_reset_done'),
-    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'),name='password_reset_confirm'),
-    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),name='password_reset_complete'),
-    
+    path('password-reset/',auth_views.PasswordResetView.as_view(),name='password_reset'),
+    path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+
 ]
