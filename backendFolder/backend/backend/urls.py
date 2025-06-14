642c1b93921a7699566827e16bc3ed6baeb0d263
Blessen Vinoy Mathew <blessenvinoymathew25@gmail.com>
2025-06-14 11:34:01 +0530

    Commited Changes

--- backendFolder/backend/backend/urls.py
+++ backendFolder/backend/backend/urls.py
@@ -33,6 +33,6 @@ urlpatterns = [
 
     path('password-reset/',auth_views.PasswordResetView.as_view(template_name ='accounts/password_reset.html'), name='password_reset'),
     path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name = 'accounts/password_reset_done.html'), name='password_reset_done'),
-    path('reset/<uidb61>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'), name='password_reset_confirm'),
     path('reset/done/',auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), name='password_reset_complete'),
 ]
