164a2eae0d3c952188a139b6419cc7669627fa82
Blessen Vinoy Mathew <blessenvinoymathew25@gmail.com>
2025-06-16 11:16:18 +0530

    Commited Changes

--- frontend/geelong_church_project/src/components/contact_us/contact.tsx
+++ frontend/geelong_church_project/src/components/contact_us/contact.tsx
@@ -201,7 +201,7 @@ export const ContactDetailsdiv = () => {
                 setmessageError('');
             }
             
-            if (valid) {
+            if (!valid) {
                 return;
             }
 
