9e9b99242f1ad05f2e7b05a25d4a9b51ae5e86ea
Blessen Vinoy Mathew <blessenvinoymathew25@gmail.com>
2025-06-18 11:08:15 +0530

    Getting ready for the deployment backend

--- backendFolder/backend/backend/settings.py
+++ backendFolder/backend/backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0n=t36e)6f4ae$7xm%f2#+(3v(-b^tyx21*_16-rusy^9*oeb)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
