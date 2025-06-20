a21e81a1b8032fa2ed404cd6df85dfe655e8be1a
Blessen Vinoy Mathew <blessenvinoymathew25@gmail.com>
2025-06-20 11:22:17 +0530

    Laravel Installation

--- backendLaravel/app/Http/Controllers/Api/ContactFormController.php
+++ backendLaravel/app/Http/Controllers/Api/ContactFormController.php
@@ -2,10 +2,24 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Contact;
 use Illuminate\Http\Request;
 
 class ContactFormController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:30',
+            'email' => 'required|email|max:50',
+            'phone_number' => 'required|string|max:20',
+            'country' => 'nullable|string|max:100',
+            'subject' => 'nullable|string|max:200',
+            'message' => 'required|string|max:500',
+        ]);
+
+       Contact::create($validated);
+
+       return response()->json(['success'=> true,'message'=> 'Message sent successfully']);
+    }
 }
