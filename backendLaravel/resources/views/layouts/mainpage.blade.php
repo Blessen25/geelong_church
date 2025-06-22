267ea782183d9e6ed3160a84cfb55b056cf4edaa
Blessen25 <blessenvinoymathew25@gmail.com>
2025-06-22 17:10:45 +0530

    Commited Chanegs

--- backendLaravel/resources/views/layouts/mainpage.blade.php
+++ backendLaravel/resources/views/layouts/mainpage.blade.php
@@ -4,9 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title','Admin Panel')</title>
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @vite(['resources/sass/app.scss', 'resources/js/app.js','resources/sass/mainpage.scss'])
 </head>
 <body>
-    <p>Main Page design</p>
+    <div class="w-100 d-flex mainpage-cstmstyle">
+        <div class="sidebar-layoutcstm">
+
+        </div>
+        <div class="main-layoutcstm">
+
+        </div>
+    </div>
 </body>
 </html>
