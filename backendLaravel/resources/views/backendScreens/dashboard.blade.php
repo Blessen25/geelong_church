f5715e51a6b7dffed83c2478fc09197d29b32627
Blessen25 <blessenvinoymathew25@gmail.com>
2025-06-23 10:54:48 +0530

    Commited Changes

--- backendLaravel/resources/views/backendScreens/dashboard.blade.php
+++
@@ -1,3 +0,0 @@
-@extends('layouts.mainpage')
-
-@section('title', 'Dashboard')
--- backendLaravel/routes/api.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use App\Http\Controllers\Api\ContactFormController;
-
-Route::post('/contact',[ContactFormController::class,'store']);
