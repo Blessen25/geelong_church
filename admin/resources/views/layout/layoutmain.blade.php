1e259f764d8cecef31f148f0534788d9dc3b2ebd
Blessen25 <blessenvinoymathew25@gmail.com>
2025-06-24 00:12:02 +0530

    commited Changes

--- admin/resources/views/layout/layoutmain.blade.php
+++ admin/resources/views/layout/layoutmain.blade.php
@@ -4,29 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../css/main.css">
     @vite(['resources/css/main.css'])
-    <style> 
-
-        .sidebar-cstm {
-
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            gap:20px
-        }
-
-        .main-cstm {
-
-            width: calc(100% - 350px);
-        }
-
-    </style>
 </head>
 <body>
     <div class="main-wrapper">
         <div class="sidebar-cstm">
-            <img src="" alt="">
+            <img src="{{ Vite::asset('resources/images/Logo_white.png') }}" alt="Logo_img">
             <a href="#">Dashboard</a>
         </div>
         <div class="main-cstm">
