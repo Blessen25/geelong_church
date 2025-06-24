ab2431f6d037ec4b8f0d1084577989f91c73ce8f
Blessen25 <blessenvinoymathew25@gmail.com>
2025-06-24 13:24:19 +0530

    Commited Cahnges

--- admin/resources/views/layout/layoutmain.blade.php
+++ admin/resources/views/layout/layoutmain.blade.php
@@ -4,16 +4,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite(['resources/css/main.css'])
 </head>
 <body>
     <div class="main-wrapper">
         <div class="sidebar-cstm">
-            <img src="{{ Vite::asset('resources/images/Logo_white.png') }}" alt="Logo_img">
-            <a href="#">Dashboard</a>
+            <img src="{{ Vite::asset('resources/images/Logo_new.png') }}" alt="Logo_img" class="Logo-imgclass">
+            <hr class="line-cstm">
+            <h1 class="sidebarh1tag">Pages</h1>
+            <a href="#" class="sidebaratag"><i class="fa-solid fa-house icontagcstm"></i><span class="sidebartexttag">Dashboard</span></a>
+            <a href="#" class="sidebaratag"><i class="fa-solid fa-address-card icontagcstm"></i><span class="sidebartexttag">Contact</span></a>
+            <a href="#" class="sidebaratag"><i class="fa-solid fa-calendar-days icontagcstm"></i><span class="sidebartexttag">Events</span></a>
+            <hr class="line-cstm">
+            <h1 class="sidebarh1tag">Profile</h1>
+            <a href="#" class="sidebaratag"><i class="fa-solid fa-user icontagcstm"></i><span class="sidebartexttag">Logout</span></a>
+
         </div>
         <div class="main-cstm">
-            @yield('content')
+            <div class="header_div">
+                <div class="pages_div">
+                    <span>Pages</span>
+                    <span>/</span>
+                    <span>@yield('page-title')</span>
+                </div>
+                <div class="profile_div">
+                    <i class="fa-regular fa-circle-user icontagcstm"></i>
+                    <span>Blessen Vinoy Mathew</span>
+                </div>
+            </div>
+            <div class="content-div">
+                @yield('content')
+            </div>
         </div>
     </div>
 </body>
