ad8caff8faf0bb22e62283db5a926c68a4fea65b
Blessen25 <blessenvinoymathew25@gmail.com>
2025-06-26 14:20:06 +0530

    Commited cahnges

--- admin/resources/views/event.blade.php
+++ admin/resources/views/event.blade.php
@@ -3,5 +3,39 @@
 @section('page-title','Event')
 
 @section('content')
-
+<h1 class="headingh1tag">Events</h1>
+    <div class="table-scroll-wrapper">
+        <table class="table-cstm">
+            <thead>
+                <tr>
+                    <th scope="col">Sl.No</th>
+                    <th scope="col">Event Name</th>
+                    <th scope="col">Event Date</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+             <tr>
+                <th scope="row">1</th>
+                <td>Mark Abraham</td>
+                <td>mark.abraham.superlongemailaddress@example.com</td>
+                <td>+1-202-555-0143</td>
+             </tr>
+             <tr>
+                <th scope="row">2</th>
+                <td>Jacob Varghese</td>
+                <td>jacob.varghese.contactmail@example.org</td>
+                <td>+91-98460-12345</td>
+                
+             </tr>
+             <tr>
+                <th scope="row">3</th>
+                <td>John Doe</td>
+                <td>john.doe.professional.email@longdomainmail.com</td>
+                <td>+44-20-7946-0958</td>
+                
+             </tr>
+         </tbody>
+        </table>
+    </div>
 @endsection
