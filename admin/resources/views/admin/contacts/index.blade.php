79556a75f924b1f67e3455f00480ec240ca9cf7d
Blessen25 <blessenvinoymathew25@gmail.com>
2025-07-02 22:06:41 +0530

    Commited Changes

--- admin/resources/views/admin/contacts/index.blade.php
+++ admin/resources/views/admin/contacts/index.blade.php
@@ -3,31 +3,35 @@
 @section('content')
 <h1 class="headingh1tag">Contact</h1>
 <div class="table-scroll-wrapper">
-    <table class="table-cstm">
-        <thead>
-            <tr>
-                <th scope="col">Sl.No</th>
-                <th scope="col">Name</th>
-                <th scope="col">Email</th>
-                <th scope="col">Phone Number</th>
-                <th scope="col">Country</th>
-                <th scope="col">Subject</th>
-                <th scope="col">Message</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($contacts as $index => $contact)
-            <tr>
-                <th scope="row">{{ $index + 1 }}</th>
-                <td>{{ $contact->name }}</td>
-                <td>{{ $contact->email }}</td>
-                <td>{{ $contact->phone_number }}</td>
-                <td>{{ $contact->country }}</td>
-                <td>{{ $contact->subject }}</td>
-                <td>{{ $contact->message }}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+     @if($contacts->isEmpty())
+        <p class="text-center p-3 paratexttable">No contact messages to display.</p>
+    @else
+        <table class="table-cstm">
+            <thead>
+                <tr>
+                    <th scope="col">Sl.No</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Phone Number</th>
+                    <th scope="col">Country</th>
+                    <th scope="col">Subject</th>
+                    <th scope="col">Message</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($contacts as $index => $contact)
+                <tr>
+                    <th scope="row">{{ $index + 1 }}</th>
+                    <td>{{ $contact->name }}</td>
+                    <td>{{ $contact->email }}</td>
+                    <td>{{ $contact->phone_number }}</td>
+                    <td>{{ $contact->country }}</td>
+                    <td>{{ $contact->subject }}</td>
+                    <td>{{ $contact->message }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
 </div>
 @endsection
--- admin/resources/views/admin/events/index.blade.php
+++ admin/resources/views/admin/events/index.blade.php
@@ -3,34 +3,38 @@
 @section('content')
 <h1 class="headingh1tag">Events</h1>
 <div class="table-scroll-wrapper">
-    <table class="table-cstm">
-        <thead>
-            <tr>
-                <th scope="col">Sl.No</th>
-                <th scope="col">Event Name</th>
-                <th scope="col">Event Date</th>
-                <th scope="col">Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($events as $index => $event)
-            <tr>
-                <th scope="row">{{ $index + 1 }}</th>
-                <td>{{ $event->event_name }}</td>
-                <td>{{ \Carbon\Carbon::parse($event->event_date)->format('d M Y') }}</td>
-                <td class="tdtableevent">
-                    <a href="{{ route('events.edit', $event->id) }}">
-                        <button type="button" class="btn btn-primary btncstmtag">Edit</button>
-                    </a>
-                    <form action="{{ route('events.destroy', $event->id) }}" method="POST" style="display:inline;">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger btncstmtag" onclick="return confirm('Are you sure?')">Delete</button>
-                    </form>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+    @if($events->isEmpty())
+        <p class="text-center p-3 paratexttable">No events have been added recently.</p>
+    @else
+        <table class="table-cstm">
+            <thead>
+                <tr>
+                    <th scope="col">Sl.No</th>
+                    <th scope="col">Event Name</th>
+                    <th scope="col">Event Date</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($events as $index => $event)
+                <tr>
+                    <th scope="row">{{ $index + 1 }}</th>
+                    <td>{{ $event->event_name }}</td>
+                    <td>{{ \Carbon\Carbon::parse($event->event_date)->format('d M Y') }}</td>
+                    <td class="tdtableevent">
+                        <a href="{{ route('events.edit', $event->id) }}">
+                            <button type="button" class="btn btn-primary btncstmtag">Edit</button>
+                        </a>
+                        <form action="{{ route('events.destroy', $event->id) }}" method="POST" style="display:inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger btncstmtag" onclick="return confirm('Are you sure?')">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
 </div>
 @endsection
