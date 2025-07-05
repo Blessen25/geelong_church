93eba82cd403e46555c115293a727af9472384a3
Blessen25 <blessenvinoymathew25@gmail.com>
2025-07-05 21:45:24 +0530

    Commited Changes

--- admin/routes/web.php
+++ admin/routes/web.php
@@ -16,3 +16,12 @@ Route::get('/admin/contacts',[ContactController::class,'index'])->name('contacts
 Route::resource('/admin/events', EventController::class)->except(['show']);
 
 Route::get('/admin/dashboard',[DashboardController::class,'index'])->name('dashboard');
+
+
+Route::get('/login',[AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/signup', [AuthController::class, 'showSignupForm'])->name('signup');
+Route::post('/signup', [AuthController::class, 'signup']);
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
