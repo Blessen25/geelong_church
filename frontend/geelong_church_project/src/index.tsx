b4f92df7381542e29c173181d5f96089bfd5ddce
Blessen25 <blessenvinoymathew25@gmail.com>
2025-09-23 15:10:48 +1000

    Commited Changes

--- frontend/geelong_church_project/src/index.tsx
+++ frontend/geelong_church_project/src/index.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Whatsappbutton } from './components/extra';
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,5 +14,6 @@ root.render(
   <React.StrictMode>
     <App />
     <Whatsappbutton />
+    <SpeedInsights />
   </React.StrictMode>
 );
